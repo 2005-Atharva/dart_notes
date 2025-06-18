d3fad40cff0935adaa3af6ac6439df10cd685468
atharvagirme <atharvagirme076@gmail.com>
2025-06-18 19:28:49 +0530

    back on track

---
+++ 9_questions.dart
@@ -0,0 +1,55 @@
+// void main() {
+//   callName('atharv', 10);
+
+//   callName2(name2: 'lol', age2: 100);
+//   callNamed3(200, 'lasa');
+// }
+
+// // 1 required position parameter
+// void callName(String name, int age) {
+//   print(name + age.toString());
+// }
+
+// // 2 required named parameter
+// void callName2({required String name2, required int age2}) {
+//   print(name2 + age2.toString());
+// }
+
+// // 3 optional position parameter
+// // ( age3 is req positional parameter and name3 is optiona positional parameter)
+// void callNamed3(int age3, [String? name3]) {
+//   print(age3.toString());
+//   if (name3 != null) {
+//     print(name3);
+//   }
+// }
+
+// // 4 optional named parameter
+
+// void callName4({String? name4}) {}
+
+// // 5 defailt parameter
+// void callName5({String name5 = 'loll'}) {}
+
+// classes
+
+// getter and setters
+
+void main() {
+  Name name = Name();
+  print(name.name2);
+  name.name09 = 'lol';
+  print(name.name2);
+}
+
+class Name {
+  String _name = 'Atharv';
+
+  String get name2 => _name;
+
+  set name09(String name3) => _name = name3;
+}
+
+
+
+// 
