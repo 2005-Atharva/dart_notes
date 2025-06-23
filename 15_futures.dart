10b289192e0f4b6c151942379a8212b3c6ae30ef
atharvagirme <atharvagirme076@gmail.com>
2025-06-23 14:11:19 +0530

    a

---
+++ 15_futures.dart
@@ -0,0 +1,25 @@
+// Exception handling only used
+
+// void main() {
+//   try {} catch (e) {
+//     print(e);
+//   }
+// }
+
+// Futures -> in js (Promises)
+
+void main() async {
+  print('Hey');
+
+  final res = await getData();
+  print(res);
+}
+
+Future<String> getData() async {
+  return Future.delayed(Duration(seconds: 2), () async {
+    return 'this is a string';
+  });
+}
+
+
+// Streams -> nothing complex only the continuous flow of data 
