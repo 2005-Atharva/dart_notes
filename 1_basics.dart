9bd328b194abe8c5e301610308b4953d6df1f97e
atharvagirme <atharvagirme076@gmail.com>
2025-06-24 20:17:04 +0530

    fix_basic

--- 1_basics.dart
+++ 1_basics.dart
@@ -1,24 +1,21 @@
 //void is the starting point of the dart
 void main() {
   print('hello dart');
-
   print(1); //-> this is also get printed
   print(
     1.runtimeType,
   ); //-> runtimetype tell which type of this variable is ( in this case int )
-
-  print(12 / 2);
-
+  print(12 / 2); // -> works
   //bool
   print(true);
 }
 
 //comments
 
-// ( / )-> 1.single line comments
+// 1.single line comments
+// ( / )->
 
 //2.
-
 /*   
  multi line commnets 
 
